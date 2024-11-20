cddf8e7f8a2c70c437cd124f680e1e1b8b363dde
Ubuntu <ubuntu@ip-192-168-11-197.ap-northeast-1.compute.internal>
2024-11-20 07:06:51 +0000

    enable airflow test connect

--- airflowbase/start.sh
+++ airflowbase/start.sh
@@ -5,6 +5,8 @@ source /root/airflow/venv/bin/activate
 # Initialize the Airflow database
 airflow db init
 
+sed -i 's/^test_connection\s*=\s*Disabled/test_connection = Enabled/' ./airflow.cfg
+
 # Create an admin user if it doesn't exist
 if ! airflow users list | grep -q "admin"; then
     airflow users create \
